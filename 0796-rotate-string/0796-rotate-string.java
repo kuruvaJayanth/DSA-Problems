081211d7e5cc1df8bbf4a23404358ebaf1f11718
Kuruva Jayanth <162041643+kuruvaJayanth@users.noreply.github.com>
2026-08-25 07:26:56 +0530

    Time: 2 ms (65.14%), Space: 43.6 MB (32.17%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         String doubled = s + s;
-        for(int i=s.length()-1;i>=0;i--) {
-            if(s.length() != goal.length()){
-                return false;
-            }
-            if(doubled.contains(goal)){
-                return true;
-            }
+        if(s.length() != goal.length()) {
+            return false;
+        }
+        if(doubled.contains(goal)) {
+            return true;
         }
         return false;
         
